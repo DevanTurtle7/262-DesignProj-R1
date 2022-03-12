16c803cd02a803d184b7e77d7711295e1ed9567f
Alison Hansen <alisonhansen@Alisons-MacBook-Pro.local>
2022-03-12 15:32:09 -0500

    artist, release, and song classes complete

---
+++ src/com/swen262/Artist.java
@@ -0,0 +1,45 @@
+package com.swen262;
+
+import java.util.UUID;
+
+public class Artist {
+    private String GUID;
+    private String name;
+    private String type;
+    private int rating;
+
+    public Artist(String name, String type, int rating){
+        this.GUID = UUID.fromString(name).toString();
+        this.name = name;
+        this.type = type;
+        this.rating = rating;
+    }
+
+    public Artist(String name, String type){
+        this.GUID = UUID.fromString(name).toString();
+        this.name = name;
+        this.type = type;
+        this.rating = 0;
+    }
+
+    public String getGUID(){
+        return this.GUID;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public int getrating(){
+        return this.rating;
+    }
+
+    @Override
+    public String toString(){
+        return (name + ", type: " + type + ", rating: " + rating);
+    }
+}
---
+++ src/com/swen262/Release.java
@@ -0,0 +1,71 @@
+package com.swen262;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.UUID;
+
+public class Release {
+    private Date issueDate;
+    private String title;
+    private Artist artist;
+    private int rating;
+    private String GUID;
+    private String medium;
+    private LinkedList<Song> tracks;
+
+    public Release(Date issueDate, String title, Artist artist, String medium, LinkedList<Song> tracks){
+        this.issueDate = issueDate;
+        this.title = title;
+        this.artist = artist;
+        this.medium = medium;
+        this.tracks = tracks;
+        this.rating = calculateRating();
+        this.GUID = UUID.fromString(title+artist.toString()+tracks.toString()).toString();
+    }
+
+    public Release(String title, Artist artist, String medium, LinkedList<Song> tracks){
+        this.issueDate = new Date();
+        this.title = title;
+        this.artist = artist;
+        this.medium = medium;
+        this.tracks = tracks;
+        this.rating = calculateRating();
+        this.GUID = UUID.fromString(title+artist.toString()+tracks.toString()).toString();
+    }
+
+    private int calculateRating(){
+        int totalRate = 0;
+        for(Song song : tracks){
+            totalRate += song.getRating();
+        }
+        return Math.round(totalRate / tracks.size());
+    }
+
+    public Date getIssueDate(){
+        return this.issueDate;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public Artist getArtist(){
+        return this.artist;
+    }
+
+    public int getRating(){
+        return this.rating;
+    }
+
+    public String getGUID(){
+        return this.GUID;
+    }
+
+    public String getMedium(){
+        return this.medium;
+    }
+
+    public LinkedList<Song> getTracks(){
+        return this.tracks;
+    }
+}
