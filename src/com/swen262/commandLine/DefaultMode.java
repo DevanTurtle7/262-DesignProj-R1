fb1d8c62eaf827262bf96565cafb055a6c05dd00
Devan Kavalchek <devankav7@gmail.com>
2022-03-13 00:42:37 -0500

    stubbed list commands

--- src/com/swen262/commandLine/DefaultMode.java
+++ src/com/swen262/commandLine/DefaultMode.java
@@ -8,11 +8,34 @@ public class DefaultMode extends Mode {
 
     @Override
     protected void listCommands() {
+        String[] message = {
+                "==========================",
+                "         COMMANDS         ",
+                "==========================",
+                "search [cat] [attr]",
+                "   Searches your personal library.",
+                "   [cat]: The type of object you're searching for (artist, song, release).",
+                "   [attr]: The attribute you're searching by (name, type, title, duration, rating, artist).",
+        };
 
+        for (String line : message) {
+            System.out.println(line);
+        }
     }
 
     @Override
     protected void handleInput(String input) {
+        String[] tokens = input.split(" ");
+        String command = tokens[0];
 
+        if (command.equals("search")) {
+
+        } else if (command.equals("add")) {
+
+        } else if (command.equals("remove")) {
+
+        } else {
+            listCommands();
+        }
     }
 }
