e48d2212aa4b74fb7b2a32ed1ebed9648edfb264
Devan Kavalchek <devankav7@gmail.com>
2022-03-14 12:19:13 -0400

    fixed database

--- src/com/swen262/Main.java
+++ src/com/swen262/Main.java
@@ -1,24 +1,17 @@
 package com.swen262;
 
-import org.apache.commons.cli.*;
-
-import java.util.Date;
-import java.util.LinkedHashSet;
+import com.swen262.database.Database;
 
 import com.swen262.commandLine.CommandLineInterface;
 
 public class Main {
 
     private static Database db;
-    private static String song_path;
-    private static String release_path;
-    private static String artist_path;
 
     public static void main(String[] args) {
-        db = new Database(song_path,release_path,artist_path);
+        db = new Database();
 
         CommandLineInterface cli = new CommandLineInterface();
         cli.run();
-
     }
 }
