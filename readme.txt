5331490e62c035b769adbdc3d0d271491a047b8f
Preston <pws1453@rit.edu>
2022-03-15 13:22:41 -0400

    prep for final submission

---
+++ readme.txt
@@ -0,0 +1,4 @@
+First, welcome to our project! This file will give you some details about our implementation.
+If asked to override files by the .bat, please select the 'N' option.
+Our source code can be found in src/ and our presentration and design pdfs can be found in docs/
+This program uses the Java17 JDK. If run with a different version, you may experience issues.
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-javac -classpath commons-cli-1.5.0.jar:commons-lang3-3.12.0.jar:opencsv-5.6.jar -d bin src/com/swen262/*.java
-java -classpath commons-cli-1.5.0.jar:commons-lang3-3.12.0.jar:opencsv-5.6.jar:bin com.swen262.Main
+javac -classpath commons-cli-1.5.0.jar:commons-lang3-3.12.0.jar:opencsv-5.6.jar -d bin src/com/swen262/*.java src/com/swen262/database/*.java src/com/swen262/DBSearches/*.java src/com/swen262/exceptions/*.java src/com/swen262/librarySearches/*.java src/com/swen262/model/*.java src/com/swen262/personalLibrary/*.java src/com/swen262/util/*.java src/com/swen262/view/*.java
+mkdir bin/com/swen262/database/data
+cp src/com/swen262/database/data/*.* bin/com/swen262/database/data/.
+java -classpath commons-cli-1.5.0.jar:commons-lang3-3.12.0.jar:opencsv-5.6.jar;bin com.swen262.Main
