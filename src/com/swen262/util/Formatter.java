fc5e565fb02a2f15afb70f1cfa7ed96d937ba1e5
Devan Kavalchek <devankav7@gmail.com>
2022-03-15 13:03:45 -0400

    added comments

--- src/com/swen262/util/Formatter.java
+++ src/com/swen262/util/Formatter.java
@@ -1,3 +1,7 @@
+/**
+ * Provides formatters for text output
+ */
+
 package com.swen262.util;
 
 public class Formatter {
@@ -6,9 +10,15 @@ public class Formatter {
     private static final int MS_IN_MINUTE = 60000;
     private static final int MS_IN_SECOND = 1000;
 
+    /**
+     * Formats a given duration
+     * @param ms The duration in milliseconds
+     * @return A string in Hh Mm Ss MSms format
+     */
     public static String formatDuration(int ms) {
         int remainingMS = ms;
 
+        // Get all the fields
         int hours = Math.floorDiv(remainingMS, MS_IN_HOUR);
         remainingMS -= hours * MS_IN_HOUR;
         int minutes = Math.floorDiv(remainingMS, MS_IN_MINUTE);
@@ -17,12 +27,15 @@ public class Formatter {
         remainingMS -= seconds * MS_IN_SECOND;
         int milliseconds = remainingMS;
 
+        // Assemble the string
         String duration = minutes + "m " + seconds + "s " + milliseconds + "ms";
 
+        // Check if hours need to be added
         if (hours > 0) {
             duration = hours + "h " + duration;
         }
 
+        // Return the string
         return duration;
     }
 }
