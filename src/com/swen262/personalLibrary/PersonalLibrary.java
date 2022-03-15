89524be261e4590c22b803673e0d2b1beab4b949
Preston <pws1453@rit.edu>
2022-03-14 21:18:54 -0400

    Added loading functionality

--- src/com/swen262/personalLibrary/PersonalLibrary.java
+++ src/com/swen262/personalLibrary/PersonalLibrary.java
@@ -1,5 +1,7 @@
 package com.swen262.personalLibrary;
 
+import com.opencsv.*;
+import com.opencsv.exceptions.CsvValidationException;
 import com.swen262.Artist;
 import com.swen262.Main;
 import com.swen262.Release;
@@ -7,12 +9,20 @@ import com.swen262.Song;
 import com.swen262.database.Database;
 
 import javax.xml.crypto.Data;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class PersonalLibrary {
-    private LinkedList<Song> songs;
-    private LinkedList<Release> releases;
+    private static LinkedList<Artist> artists;
+    private static LinkedList<Song> songs;
+    private static LinkedList<Release> releases;
+    private static HashMap<Song, Release> songsToRelease;
 
     private static PersonalLibrary activeInstance;
 
@@ -20,16 +30,133 @@ public class PersonalLibrary {
         activeInstance = this;
         this.songs = songs;
         this.releases = releases;
+        songsToRelease = new HashMap<>();
     }
 
     public static PersonalLibrary getActiveInstance() {
         if (activeInstance == null) {
-            // TODO: Load a personal library if it exists. otherwise create a new one
+            try{
+                // TODO: Load a personal library if it exists. otherwise create a new one
+                File artistCSV = new File("src/com/swen262/personalLibrary/data/artists.csv");
+                File songCSV = new File("src/com/swen262/personalLibrary/data/songs.csv");
+                File releaseCSV = new File("src/com/swen262/personalLibrary/data/releases.csv");
+                if (songCSV.createNewFile() && releaseCSV.createNewFile() && artistCSV.createNewFile()) {
+                    // create new release and songVs
+                    return new PersonalLibrary();
+                } else {
+                    RFC4180Parser rfcDoubleParse = new RFC4180ParserBuilder().withQuoteChar('\"').build();
 
-            return new PersonalLibrary();
+                    //artists
+                    artists = new LinkedList<Artist>();
+                    CSVReader artist_csv_reader = new CSVReaderBuilder(new FileReader(artistCSV)).withCSVParser(rfcDoubleParse).build();
+                    String[] artist_csv_record;
+
+                    while ((artist_csv_record = artist_csv_reader.readNext()) != null){
+                        Artist curArtist = artistParser(artist_csv_record);
+                        artists.add(curArtist);
+                    }
+
+                    //songs
+                    CSVReader song_csv_reader =  new CSVReaderBuilder(new FileReader(songCSV)).withCSVParser(rfcDoubleParse).build();
+                    String[] song_csv_record;
+                    while ((song_csv_record = song_csv_reader.readNext()) != null){
+                        Song curSong = songParser(song_csv_record);
+                        songs.add(curSong);
+                        songsToRelease.put(curSong, null);
+                    }
+
+                    CSVReader release_csv_reader = new CSVReaderBuilder(new FileReader(releaseCSV)).withCSVParser(rfcDoubleParse).build();
+                    String[] release_csv_record;
+                    while ((release_csv_record = release_csv_reader.readNext()) != null){
+                        Release curRelease = releaseParser(release_csv_record);
+                        releases.add(curRelease);
+                    }
+
+                    return new PersonalLibrary(songs,releases);
+                }
+            } catch (IOException | CsvValidationException e){
+               e.printStackTrace();
+            }
         } else {
             return activeInstance;
         }
+        return null;
+    }
+
+    private static Artist artistParser(String[] artist_attributes){
+        String guid = artist_attributes[0];
+        String name = artist_attributes[1];
+        String type = artist_attributes[2];
+        return new Artist(guid,name,type);
+    }
+
+    public static Artist searchArtistByGUID(String GUID){
+        for (Artist artist:artists) {
+            if (GUID.equals(artist.getGUID())){
+                return artist;
+            }
+        }
+        return null;
+    }
+
+    private static Song songParser(String[] song_attributes){
+
+        String song_guid = song_attributes[0];
+        String artist_guid = song_attributes[1];
+        Artist artist = searchArtistByGUID(artist_guid);
+        int duration = Integer.parseInt(song_attributes[2]);
+        String title = song_attributes[3];
+        return new Song(title,artist,duration,song_guid);
+    }
+
+    private static Song searchSongByGUID(String GUID){
+        for (Song song: songs) {
+            if (GUID.equals(song.getGUID())){
+                return song;
+            }
+        }
+        return null;
+    }
+
+    private static Release releaseParser(String[] release_attributes){
+        int length = release_attributes.length;
+        String release_guid = release_attributes[0];
+        String artist_guid = release_attributes[1];
+        Artist artist = searchArtistByGUID(artist_guid);
+        String title = release_attributes[2];
+        String issue_date = release_attributes[4];
+        //System.out.println("Original issue date: "+issue_date);
+        String medium = release_attributes[3];
+        if (issue_date.matches("^\\d\\d\\d\\d$")){
+            issue_date += "-01-01";
+        }
+        if (issue_date.matches("^\\d\\d\\d\\d-\\d\\d$")){
+            issue_date += "-01";
+        }
+        //System.out.println(issue_date);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date iss_date;
+        LinkedHashSet<Song> tracks = new LinkedHashSet<>();
+        for (int i = 5; i < length ; i++) {
+            String trackGUID = release_attributes[i];
+            //System.out.println(track);
+            Song track = searchSongByGUID(trackGUID);
+            tracks.add(track);
+        }
+        try{
+            iss_date = formatter.parse(issue_date);
+            Release release = new Release(iss_date,title,artist,medium,tracks,release_guid);
+
+            for (Song song : tracks) {
+                songsToRelease.replace(song, release);
+            }
+
+            return release;
+        } catch (ParseException e){
+            System.out.println("ohno error");
+            e.getMessage();
+        }
+        return null;
     }
 
     public PersonalLibrary(){
