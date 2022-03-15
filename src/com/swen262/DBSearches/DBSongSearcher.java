e3284409ea209159b7c049b61d4b019488dc3092
johnaflory <johnaflory@gmail.com>
2022-03-15 00:54:36 -0400

    Small comments for Strategy interfaces

--- src/com/swen262/DBSearches/DBSongSearcher.java
+++ src/com/swen262/DBSearches/DBSongSearcher.java
@@ -2,6 +2,11 @@ package com.swen262.DBSearches;
 
 import java.util.LinkedList;
 
+/**
+ * An interface to be inherited by a concrete strategy to allow
+ * for searching of the Database for Artists, Releases, and Songs
+ * given query information
+ */
 public interface DBSongSearcher<E> {
     LinkedList<E> algorithm(String query);
 }
--- src/com/swen262/librarySearches/LibrarySongSearcher.java
+++ src/com/swen262/librarySearches/LibrarySongSearcher.java
@@ -2,6 +2,11 @@ package com.swen262.librarySearches;
 
 import java.util.LinkedList;
 
+/**
+ * An interface to be inherited by a concrete strategy to allow
+ * for searching of the PersonalLibrary for Artists, Releases, and Songs
+ * given query information
+ */
 public interface LibrarySongSearcher<E> {
     LinkedList<E> algorithm(String query);
 }
